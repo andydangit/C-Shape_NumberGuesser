89f093350128f070c0fcd273c263f87b8d035b7b
andydangit <andy.dang.it@gmail.com>
2021-11-09 14:13:43 -0600

    Remove not needed line of code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace NumberGuesser
 {
@@ -10,7 +7,7 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine( "testing");
+            
         }
     }
 }
