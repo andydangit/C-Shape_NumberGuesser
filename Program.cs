1dc40c10db135df097d7d45c7f698bcb80bab44e
andydangit <andy.dang.it@gmail.com>
2021-11-09 15:10:17 -0600

    matching the correct number from user input

--- Program.cs
+++ Program.cs
@@ -28,9 +28,9 @@ namespace NumberGuesser
             Console.WriteLine("What is your name?");
 
             // get user input
-            string input = Console.ReadLine();
+            string inputName = Console.ReadLine();
 
-            Console.WriteLine("Hello {0}, let's play a game...", input );
+            Console.WriteLine("Hello {0}, let's play a game...", inputName );
 
             // Init correct number
             int correctNumber = 7;
@@ -38,8 +38,43 @@ namespace NumberGuesser
             // Init guess var
             int guess = 0;
 
+            //Ask user for number
             Console.WriteLine("Guess a number between 1 and 10");
 
+            //while guess is not correct
+            while (guess != correctNumber)
+            {
+                // Get users input
+                string input = Console.ReadLine();
+
+                // Cast to int and put in guess
+                guess = Int32.Parse(input);
+
+                // Match guess to correct number
+                if(guess != correctNumber)
+                {
+                    //Change text color
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                    // Write out app info 
+                    Console.WriteLine("Wrong number, please try again");
+
+                    // Reset text color
+                    Console.ResetColor();
+                }
+
+            }
+
+            // Output sucess message
+
+            //Change text color
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            // Write out app info 
+            Console.WriteLine("Wrong number, please try again");
+
+            // Reset text color
+            Console.ResetColor();
 
         }
     }
