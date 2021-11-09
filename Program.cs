2d082ce6729686ba6b42d106ecfbdbb340664902
andydangit <andy.dang.it@gmail.com>
2021-11-09 14:17:59 -0600

    adding comment

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
-
+//Namespace
 namespace NumberGuesser
 {
+    //Main Class
     class Program
     {
+           //Entry Point Method
         static void Main(string[] args)
         {
-            
+            //Start Here //
+
+            Console.WriteLine("Hello World");
         }
     }
 }
