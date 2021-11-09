892b066d9c8351b1558bfd6c6018c7a5b8ac14d4
andydangit <andy.dang.it@gmail.com>
2021-11-09 14:46:16 -0600

    Add text color and reset it back to white

--- Program.cs
+++ Program.cs
@@ -15,7 +15,14 @@ namespace NumberGuesser
             var appVersion = "1.0.0";
             var appAuthor = "Andy Dang";
 
+            //Change text color
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            //Write out app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor );
+
+            //reset text color 
+            Console.ResetColor();
         }
     }
 }
