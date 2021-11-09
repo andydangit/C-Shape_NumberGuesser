1f7dbb916605f78bdc0340513b30bea0d2b59917
andydangit <andy.dang.it@gmail.com>
2021-11-09 15:46:40 -0600

    refactor

--- Program.cs
+++ Program.cs
@@ -10,19 +10,7 @@ namespace NumberGuesser
         static void Main(string[] args)
         {
 
-            // Set app vars
-            var appName = "Number Guesser";
-            var appVersion = "1.0.0";
-            var appAuthor = "Andy Dang";
-
-            // Change text color
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-
-            // Write out app info
-            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor );
-
-            // reset text color 
-            Console.ResetColor();
+            GetAppInfo(); // Run GetAppInfo function
 
             // Ask the user name
             Console.WriteLine("What is your name?");
@@ -123,6 +111,23 @@ namespace NumberGuesser
 
             }
 
+        }
+
+        static void GetAppInfo()
+        {
+            // Set app vars
+            var appName = "Number Guesser";
+            var appVersion = "1.0.0";
+            var appAuthor = "Andy Dang";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // reset text color 
+            Console.ResetColor();
         }
     }
 }
